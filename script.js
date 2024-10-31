92a75a82c8435a0acf07df92fde51fe944af3c21
marita1234567 <m.bibichadze@sabauni.edu.ge>
2024-11-01 00:11:55 +0400

    work19/2

--- script.js
+++ script.js
@@ -1,33 +1,23 @@
 let addButton = document.getElementById("btn");
-      
 
 function addelement(addinfo) {
   let promtt = prompt("ჩაწერეთ სასურველი სიტყვა");
-  
+
   if (!promtt) {
-    alert("გამარჯობა");
-    
- 
-
-    
-  } 
-
-  let ul = document.getElementById("addlii");
-  let li = document.createElement("li");
-  li.textContent = promtt;
-
-
-  let Button1 = document.createElement("button");
-  Button1.textContent = "delete";
-  Button1.click = function(deleteinfo) {
-    ul.removeChild(li);
-  li.appendChild(button1);
- 
+    alert("shevset veili");
+  } else {
+    let ul = document.getElementById("addlii");
+    let li = document.createElement("li");
+    li.textContent = promtt;
+    ul.appendChild(li);
+    let removeBtn = document.createElement("button");
+    removeBtn.textContent = "remove";
+    li.appendChild(removeBtn);
+    removeBtn.addEventListener("click", () => {
+      ul.removeChild(li);
+    });
     
   }
-
-
 }
-
 
 addButton.addEventListener("click", addelement);
